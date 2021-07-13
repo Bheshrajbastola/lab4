9db8b25a944b20de7bc548bc00a02c6974d41dc0
dell <BOB-ctrl-creator>
2021-07-13 23:39:10 +0545

    update

---
+++ Dictionary/qn5.py
@@ -0,0 +1,5 @@
+'''.Write a Python program to iterate over dictionaries using for loops.'''
+d1={1:2,2:1}
+for i in d1:
+    print(d1)
+    
--- qn9.py
+++ qn9.py
@@ -1,3 +1,9 @@
 '''Write a program to find the factorial of a number.'''
-
+def fact(num):
+    if num<=1:
+        return 1
+    else:
+        return num*fact(num-1)
+ans=fact(5)
+print("fact of given num is : ",ans)
 
